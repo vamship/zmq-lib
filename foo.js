fe1c4b868a71bbd5f0a7c382fbf5e4907a7d6dde
vamship <vamshi.ponnapalli@gmail.com>
2014-11-10 14:12:27 -0500

    Removed temporary test file

--- foo.js
+++ foo.js
@@ -1,20 +1,7 @@
-var _zmq = require('zmq');
 
-
-
-var ep = 'ipc://foo';
-var client = _zmq.createSocket('req');
-var server = _zmq.createSocket('rep');
-
-server.monitor(10);
-server.on('accept', function(){
-    console.log('ACCEPT!');
-});
-
-server.bind(ep);
-client.connect(ep);
+console.log('Started');
 
 setTimeout(function() {
-    console.log('Timeout expired!');
-    process.exit();
-}, 2000);
+    console.log('Throwing error');
+    throw new Error('Something went wrong!');
+}, 5000);
