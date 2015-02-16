e61726260ba3b3e6c26264133ca3cca73f747039
vamship <vamshi.ponnapalli@gmail.com>
2015-02-16 10:48:40 -0500

    Removed commented out line (jshint) on default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -196,7 +196,7 @@ module.exports = function(grunt) {
      *  - Cleaning up build results
      */
     grunt.registerTask('default', [ 'jsbeautifier:dev',
-                                    //'jshint:dev', //TODO: Keeping this here causes the task to break (segmentation fault). Needs review.
+                                    'jshint:dev',
                                     'test:unit',
                                     'clean' ]);
 
